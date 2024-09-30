99baf89c00ad4a3be605b5622336bd3e6e7ff70a
rongrong <tf01017841@gmail.com>
2024-09-30 23:21:10 +0800

    update week-03 callback.js

--- week-03/callback.js
+++ week-03/callback.js
@@ -8,24 +8,7 @@ function doJob(job, time, cb) {
 
 // 目標輸出：刷牙 1sec -> 吃早餐 3 sec -> 寫功課 1sec -> 吃午餐 2sec
 
-/*法一：超笨方法，直接把要等待的時間加總
-let now = new Date();
-console.log(`開始工作 at ${now.toISOString()}`);
-
-doJob('刷牙', 1000, function (data) { // 等1sec
-    console.log(data);
-});
-doJob('吃早餐', 4000, function (data) { // 等(1+3)sec，以此類推
-    console.log(data);
-});
-doJob('寫功課', 5000, function (data) {
-    console.log(data);
-});
-doJob('吃午餐', 7000, function (data) {
-    console.log(data);
-});*/
-
-//法二：每個job放在前一個job的callback函數裡面執行，依序觸發
+//每個job放在前一個job的callback函數裡面執行，依序觸發
 let now = new Date();
 console.log(`開始工作 at ${now.toISOString()}`);
 
@@ -41,8 +24,6 @@ doJob('刷牙', 1000, function (data) {
         });
     });     
 });
-
-//法三：
 
 
 
